018a504a86e1abf646b184dea4cfb377d938d978
winnip <pangwentao_pwt@126.com>
2017-02-24 11:09:08 +0800

    [tutorial01] answer 2

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -60,7 +60,14 @@ int lept_parse(lept_value* v, const char* json) {
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+	int ret = lept_parse_value(&c, v);
+	if ( ret == LEPT_PARSE_OK)
+	{
+		lept_parse_whitespace(&c);
+		if (*c.json != '\0')
+			return LEPT_PARSE_ROOT_NOT_SINGULAR;
+	}
+    return ret;
 }
 
 lept_type lept_get_type(const lept_value* v) {
--- tutorial01/test.c
+++ tutorial01/test.c
@@ -85,7 +85,5 @@ static void test_parse() {
 int main() {
     test_parse();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
-	char a;
-	scanf("%c", &a);
     return main_ret;
 }
